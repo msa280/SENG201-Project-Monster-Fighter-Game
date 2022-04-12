2208c8dbc6ae0bcf815966ca899374d5ca60287b
VendrickNZ <jakibisherwood88@gmail.com>
2022-04-13 01:22:24 +1200

    added player class

---
+++ Monster Fighter/src/Player.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+public class Player {
+	
+	private int playerGold;
+	private HashMap<String, String> playerInventory = new HashMap<String, String>(); 
+	private ArrayList<Monster> playersTeam = new ArrayList<Monster>();
+
+}

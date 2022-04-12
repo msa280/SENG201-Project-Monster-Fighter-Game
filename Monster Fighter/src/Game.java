f454a3c6a707ec8714fccb82451d07eed9d3d15f
Haider <83687715+msa280@users.noreply.github.com>
2022-04-13 00:12:40 +1200

    Added more functions to Game class.

--- Monster Fighter/src/Game.java
+++ Monster Fighter/src/Game.java
@@ -6,79 +6,238 @@
  * 
  */
 
-import java.util.Scanner;  // Import the Scanner class 
+import java.util.InputMismatchException;
+import java.util.Scanner;  // Imports
 import java.util.regex.Matcher; 
 import java.util.regex.Pattern;
 
 
 public class Game {
 
-	/**
-	 * @param args
-	 * 
-	 */
-	
-	
+	private Scanner scan_input = new Scanner(System.in);
 	private String playerName;
+	private int gameLength = 5;
+	private double gameDifficulty = 0.0;
+	private boolean gameOver = false;
 	
 	
-	public void setPlayerName() {
-		
-		boolean name_check_passed = false;
-		Scanner scan_name = new Scanner(System.in); //Asks player name 
-		System.out.print("Enter your fighter name: ");
-		String scanned_name = scan_name.nextLine();
-		
-		int name_length = scanned_name.length();
-		Pattern pattern = Pattern.compile("[^a-zA-Z]");
-		Matcher matcher = pattern.matcher(scanned_name);
-		boolean name_has_num_or_special_character = matcher.find();
-		
-		
-		while (name_check_passed == false) {
-			
-			if (name_has_num_or_special_character) {
-				System.out.println(scanned_name + " contains a number or special character!\n");
-				System.out.println("Please choose a different fighter name.\n");
-				break;
-			} 
-			else if (name_length < 3) {
-				System.out.println(scanned_name + " is too short!\n");
-				System.out.println("Please choose a longer fighter name.\n");
-				break;
-			} 
-			else if (name_length > 15) {
-				System.out.println(scanned_name + " is too long!\n");
-				System.out.println("Please choose a shorter fighter name.\n");
-				setPlayerName();
-				break;
-			}
-			else {
-				name_check_passed = true;
-			}
-		}
-		
+	public String getPlayerName() 
+	{
+		return playerName;
+	}
 
-		if (name_check_passed == true) {
-			this.playerName = scanned_name;
+	public void setPlayerName(String playerName) 
+	{
+		this.playerName = playerName;
+	}
+	
+	public int getGameLength() 
+	{
+		return gameLength;
+	}
+
+	public void setGameLength(int gameLength)
+	{
+		this.gameLength = gameLength;
+	}
+	
+	public double getGameDifficulty() 
+	{
+		return gameDifficulty;
+	}
+
+	public void setGameDifficulty(double gameDifficulty)
+	{
+		this.gameDifficulty = gameDifficulty;
+	}
+	
+	public boolean isGameOver() {
+		return gameOver;
+	}
+
+	public void setGameOver(boolean gameOver) {
+		this.gameOver = gameOver;
+	}
+
+
+
+	public boolean check_name(boolean num_or_special_char, boolean name_check_passed, int name_length, String scanned_name) 
+	{
+		if (num_or_special_char) 
+		{
+			System.out.println(scanned_name + " contains numbers or special characters!\n");
+			System.out.println("Please choose a different fighter name.\n");
 		} 
-		else {
-			this.setPlayerName();
+		else if (name_length < 3) 
+		{
+			System.out.println(scanned_name + " is too short!\n");
+			System.out.println("Please choose a longer fighter name.\n");
+		} 
+		else if (name_length > 15) 
+		{
+			System.out.println(scanned_name + " is too long!\n");
+			System.out.println("Please choose a shorter fighter name.\n");
+		}
+		else 
+		{
+			name_check_passed = true;
+		}
+		return name_check_passed;
+	}
+	
+	
+	public void ask_PlayerName() 
+	{
+		
+		Pattern pattern = Pattern.compile("[^a-zA-Z]");
+		String scanned_name = null;
+		Matcher matcher = null;
+		int name_length = 0;
+		boolean num_or_special_char = false;
+		boolean name_check_passed = false;
+		
+		while (name_check_passed == false) 
+		{	
+			System.out.print("Enter your fighter name: ");
+			scanned_name = scan_input.nextLine();
+			name_length = scanned_name.length();
+			matcher = pattern.matcher(scanned_name);
+			num_or_special_char = matcher.find();
+			name_check_passed = check_name(num_or_special_char, name_check_passed, name_length, scanned_name);
+		}
+		this.setPlayerName(scanned_name);
+	}
+	
+	
+	public boolean check_gameLength(boolean gameLength_check_passed, int scanned_length) 
+	{
+		
+		if (scanned_length < 5) 
+		{
+			System.out.println(scanned_length + " is too short!\n");
+			System.out.println("Please choose a number of days between 5 and 15.\n");
+		} 
+		else if (scanned_length > 15) 
+		{
+			System.out.println(scanned_length + " is too long!\n");
+			System.out.println("Please choose a number of days between 5 and 15.\n");
+		}
+		else 
+		{
+			gameLength_check_passed = true;
+		}
+		return gameLength_check_passed;
+	}
+	
+	
+	public void ask_GameLength() 
+	{
+		
+		int scanned_length = 0;
+		boolean gameLength_check_passed = false;
+		
+		while (gameLength_check_passed == false) {
 			
+			System.out.print("Enter the number of days you want to play: ");
+			
+			try 
+			{
+				scanned_length = scan_input.nextInt();
+				gameLength_check_passed = check_gameLength(gameLength_check_passed, scanned_length);
+			} 
+			catch (InputMismatchException excp) 
+			{
+				System.out.print("Game length should be a number between 5 and 15.\n");
+				scan_input.next();
+				continue;
+			}
+		}
+		
+		this.setGameLength(scanned_length);
+	}
+	
+	
+	public boolean check_and_set_gameDifficulty(boolean valid_option, int option_number) 
+	{
+		if (option_number == 1) 
+		{
+			this.setGameDifficulty(1.0);
+			valid_option = true;
+        } 
+		else if (option_number == 2) 
+        {
+			this.setGameDifficulty(0.5);
+			valid_option = true;
+        } 
+		else if (option_number == 3) 
+		{
+			this.setGameDifficulty(1.5);
+			valid_option = true;
+        } 
+		else if (option_number == 4) 
+		{
+			this.setGameDifficulty(2.0);
+			valid_option = true;
+        }
+		else if (option_number == 5) 
+		{
+			this.setGameDifficulty(3.0);
+			valid_option = true;
+        }
+		else 
+		{
+			System.out.print("Choose an option number between 1 and 5.\n");
+		}
+		return valid_option;
+		
+	}
+	
+	
+	public void ask_GameDifficulty() 
+	{
+		int option_number = 0;
+		boolean valid_option = false;
+		
+	    while (valid_option == false) 
+	    {
+			
+	    	System.out.println("What is your preferred game difficulty?");
+		    System.out.println("1. Classic");
+		    System.out.println("2. Easy");
+		    System.out.println("3. Normal");
+		    System.out.println("4. Hard");
+		    System.out.println("5. Boss");
+			
+			try 
+			{
+				option_number = scan_input.nextInt();
+				valid_option = check_and_set_gameDifficulty(valid_option, option_number);
+			} 
+			catch (InputMismatchException excp) 
+			{
+				System.out.print("Please enter a valid option number.\n");
+				scan_input.next();
+				continue;
+			}
 		}
 	}
 	
 	
 	
-	public static void main(String[] args) {
-		
-		Game new_game = new Game();
-		new_game.setPlayerName();
-
-	}
-
-
-
 	
-
+	
+	public static void main(String[] args)
+	{
+		Game new_game = new Game();
+		new_game.ask_PlayerName();
+		new_game.ask_GameLength();
+		new_game.ask_GameDifficulty();
+		
+		while (!new_game.isGameOver()) 
+		{
+			System.out.print("NOW GAME STARTS! Add functions inside main game!");
+			// do game
+			break;
+		}
+	}
 }

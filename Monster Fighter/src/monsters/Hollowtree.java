6c695c7af15e08867fe57ef2238ad9d79b553a71
Haider <83687715+msa280@users.noreply.github.com>
2022-04-14 00:30:06 +1200

    Added monster package

---
+++ Monster Fighter/src/monsters/Hollowtree.java
@@ -0,0 +1,10 @@
+package monsters;
+
+public class Hollowtree extends Monster {
+
+	public Hollowtree() {
+		
+		super("Hollowtree", "Branch slice", "Wasp Control", 100, 10, 20, 100, 30);
+	}
+
+}
---
+++ Monster Fighter/src/monsters/Mornpest.java
@@ -0,0 +1,11 @@
+package monsters;
+
+public class Mornpest extends Monster {
+
+	public Mornpest() {
+		
+		super("Mornpest", "Pest fire", "Dissolve", 100, 10, 20, 100, 30);
+	
+	}
+
+}

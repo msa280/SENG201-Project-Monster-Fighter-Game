5e151ea4a094b74e196aa17498a9be315412676f
Haider <83687715+msa280@users.noreply.github.com>
2022-04-14 00:29:45 +1200

    Delete Monster Fighter/src/Monster directory

--- Monster Fighter/src/Monster/Soilscreamer.java
+++
@@ -1,8 +0,0 @@
-package Monster;
-
-public class Soilscreamer extends Monster {
-
-	public Soilscreamer() {
-		super("Soilscreamer", "Soil Splash", "Worm Kill", 100, 10, 20, 100, 30);
-	}
-}
--- Monster Fighter/src/Monster/Venomhound.java
+++
@@ -1,9 +0,0 @@
-package Monster;
-
-public class Venomhound extends Monster{
-
-	public Venomhound() {
-		super("Venomhound", "Poison spit", "Death Grip", 100, 10, 20, 100, 30);
-	}
-
-}

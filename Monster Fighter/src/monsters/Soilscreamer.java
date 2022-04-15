c34d1ef6fc4c5ec33097de8a0d82e01561a36238
Haider <83687715+msa280@users.noreply.github.com>
2022-04-15 21:53:55 +1200

    Delete Monster Fighter directory

--- Monster Fighter/src/monsters/Soilscreamer.java
+++
@@ -1,8 +0,0 @@
-package monsters;
-
-public class Soilscreamer extends Monster {
-
-	public Soilscreamer() {
-		super("Soilscreamer", null, "Soil Splash", "Worm Kill", 100, 10, 20, 100, 30, 300);
-	}
-}

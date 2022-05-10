d6630ebc87531481754396b0739c65aad504d765
Jakib Isherwood <jis48@cs22069iu.canterbury.ac.nz>
2022-05-11 11:12:35 +1200

    commenting

--- src/Enemy.java
+++ src/Enemy.java
@@ -8,52 +8,104 @@ import monsters.Mornpest;
 import monsters.Soilscreamer;
 import monsters.Venomhound;
 
+/*
+ * Generates enemies, and their monster teams.
+ */
+
 public class Enemy {
 	
+	/*
+	 *  The name of the Enemy.
+	 */
 	private String enemyName;
+	/*
+	 * The enemies team, which is a list of Monsters.
+	 */
 	private ArrayList<Monster> enemyTeam = new ArrayList<Monster>();
+	/*
+	 * Whether the enemy has already been fought or not.
+	 */
 	private boolean alreadyFought = false;
-	
+	/*
+	 * A list of Monsters.
+	 */
 	private Monster allMonsters[] = new Monster[5];
+	/*
+	 * All of the names of the enemies.
+	 */
 	private String allEnemyNames[] = {"Angela", "Arnold", "Cameron", "Julian", "Bladee", "Thaiboy", "Jonathan", "Mark", "Joshua", "Andrew"};
 	
 	
-	
+	/*
+	 * Sets the value of haveFought.
+	 * 
+	 * @param haveFought Whether the enemy has already fought the player or not.
+	 */
 	public void setEnemyFightAvailability(boolean haveFought)
 	{
 		this.alreadyFought = haveFought;
 	}
 	
-	public void setEnemyName(String name)
+	/*
+	 * Sets the value of enemyName.
+	 * 
+	 * @param enemyName The name of the Enemy.
+	 */
+	public void setEnemyName(String enemyName)
 	{
-		this.enemyName = name;
+		this.enemyName = enemyName;
 	}
 	
+	/*
+	 * Gets the enemies name.
+	 * 
+	 * @return Returns the value of enemyName.
+	 */
 	public String getEnemyName()
 	{
 		return this.enemyName;
 	}
 	
+	/*
+	 * Gets whether the enemy has been fought or not already.
+	 * 
+	 * @return Returns the value of alreadyFought.
+	 */
 	public boolean getEnemyFightAvailability()
 	{
 		return this.alreadyFought;
 	}
 	
+	/*
+	 * Gets the enemies team which consists of Monsters.
+	 * 
+	 * @return Returns the enemyTeam array.
+	 */
 	public ArrayList<Monster> getEnemyTeam()
 	{
 		return this.enemyTeam;
 	}
 	
+	/*
+	 * Sets the value of alreadyFought.
+	 * 
+	 * @param alreadyFought Whether or not the player has already fought the enemy.
+	 */
 	public void setAlreadyFought(boolean alreadyFought) {
 		this.alreadyFought = alreadyFought;
 	}
 	
+	/*
+	 * Gets whether or not the enemy has already been fought. If they have,
+	 * returns true, else false.
+	 */
 	public boolean getAlreadyFought() {
 		return alreadyFought;
 	}
 	
-	
-	
+	/*
+	 * Generates the enemy monsters used to create their teams.
+	 */
 	public void generateEnemyMonsters()
 	{
 		this.allMonsters[0] = new Cavernfreak();
@@ -63,7 +115,12 @@ public class Enemy {
 		this.allMonsters[4] = new Venomhound();
 	}
 	
-	
+	/*
+	 * Generates the enemies name by randomly picking
+	 * a position in a list of names.
+	 * 
+	 * @return Returns the enemies name.
+	 */
 	public String generateEnemyName()
 	{
 		Random randNum = new Random();
@@ -75,7 +132,9 @@ public class Enemy {
 	}
 	
 	
-	
+	/*
+	 * Generates enemy teams.
+	 */
 	public void generateEnemyTeam()
 	{
 		boolean generationComplete = false;

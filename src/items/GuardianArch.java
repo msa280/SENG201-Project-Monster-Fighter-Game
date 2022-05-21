f98cc9be6b703a732592bf9f61fb0a17ae92d781
VendrickNZ <jakibisherwood88@gmail.com>
2022-05-21 23:08:19 +1200

    commented

--- src/items/GuardianArch.java
+++ src/items/GuardianArch.java
@@ -2,14 +2,25 @@ package items;
 
 import monsters.Monster;
 
+/*
+ * Creates an item in the game.
+ */
 public class GuardianArch extends Item
 {
-
+	
+	/*
+	 * Creates the item and specifies what it does.
+	 */
 	public GuardianArch() 
 	{
 		super("Guardian Arch", "Increases monster's healing by 20.", 150);
 	}
 	
+	/*
+	 * Uses the item.
+	 * 
+	 * @param monster The monster to use the item on.
+	 */
 	public void useGuardianArch(Monster monster)
 	{
 		monster.setHealAmount(monster.getHealAmount() + 20);

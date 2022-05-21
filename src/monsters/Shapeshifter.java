42b235f045ceff5d2dd6e838c4ca6fc04f6cbfa5
VendrickNZ <jakibisherwood88@gmail.com>
2022-05-21 23:32:54 +1200

    commented

--- src/monsters/Shapeshifter.java
+++ src/monsters/Shapeshifter.java
@@ -1,7 +1,13 @@
 package monsters;
 
+/*
+ * Creates a monster in the game.
+ */
 public class Shapeshifter extends Monster {
 
+	/*
+	 * The monster that is being created.
+	 */
 	public Shapeshifter() {
 		super("ShapesShifter", null, "Electrolysis", "Shape Shift", 125, 35, 35, 125, 35, 450, false, 4);
 	}

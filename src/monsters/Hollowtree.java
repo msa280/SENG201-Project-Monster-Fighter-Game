54d1087701c349c0ff5d6833fc510009ecc20965
VendrickNZ <jakibisherwood88@gmail.com>
2022-05-21 23:32:24 +1200

    commented

--- src/monsters/Hollowtree.java
+++ src/monsters/Hollowtree.java
@@ -1,7 +1,13 @@
 package monsters;
 
+/*
+ * Creates a monster in the game.
+ */
 public class Hollowtree extends Monster {
-
+	
+	/*
+	 * The monster that is being created.
+	 */
 	public Hollowtree() {
 		
 		super("Hollowtree", null, "Branch slice", "Wasp Control", 100, 10, 20, 100, 30, 300, false, 0);

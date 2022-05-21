4f297727b1cffcba260902f286b3d414a15caa51
VendrickNZ <jakibisherwood88@gmail.com>
2022-05-21 23:32:49 +1200

    commented

--- src/monsters/Mornpest.java
+++ src/monsters/Mornpest.java
@@ -1,7 +1,13 @@
 package monsters;
 
+/*
+ * Creates a monster in the game.
+ */
 public class Mornpest extends Monster {
 
+	/*
+	 * The monster that is being created.
+	 */
 	public Mornpest() {
 		
 		super("Mornpest", null, "Pest fire", "Dissolve", 100, 10, 20, 100, 30, 300, false, 0);

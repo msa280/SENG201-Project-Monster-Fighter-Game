c3c479f29957ab631303a919fb73ca764a28939a
VendrickNZ <jakibisherwood88@gmail.com>
2022-05-21 23:33:02 +1200

    commented

--- src/monsters/Soilscreamer.java
+++ src/monsters/Soilscreamer.java
@@ -1,7 +1,13 @@
 package monsters;
 
+/*
+ * Creates a monster in the game.
+ */
 public class Soilscreamer extends Monster {
 
+	/*
+	 * The monster that is being created.
+	 */
 	public Soilscreamer() {
 		super("Soilscreamer", null, "Soil Splash", "Worm Kill", 100, 10, 20, 100, 30, 300, false, 0);
 	}

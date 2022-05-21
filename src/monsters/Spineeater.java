c3632503bc2b16042b2624858649f26d706d61a6
VendrickNZ <jakibisherwood88@gmail.com>
2022-05-21 23:33:09 +1200

    commented

--- src/monsters/Spineeater.java
+++ src/monsters/Spineeater.java
@@ -1,7 +1,13 @@
 package monsters;
 
+/*
+ * Creates a monster in the game.
+ */
 public class Spineeater extends Monster {
 
+	/*
+	 * The monster that is being created.
+	 */
 	public Spineeater() {
 		super("SpineEater", null, "Thorn Slash", "Spine Break", 200, 40, 100, 200, 40, 800, false, 2);
 	}

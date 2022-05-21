b6c453faafd46f9dfa6b16f9f725ff8f04ce45ce
VendrickNZ <jakibisherwood88@gmail.com>
2022-05-21 23:32:27 +1200

    commented

--- src/monsters/Magmataur.java
+++ src/monsters/Magmataur.java
@@ -1,7 +1,13 @@
 package monsters;
 
+/*
+ * Creates a monster in the game.
+ */
 public class Magmataur extends Monster {
 	
+	/*
+	 * The monster that is being created.
+	 */
 	public Magmataur() {
 		super("Magmataur", null, "Bull Charge", "Fire Ball", 100, 10, 20, 100, 30, 300, false, 0);
 	}
